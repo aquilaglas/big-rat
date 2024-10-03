4d6437009a144023cb0a7ffc0b4afa399b724ef7
aquilaglas <arthur.reant@supdevinci-edu.fr>
2024-10-03 16:06:12 +0200

    add: base to BMAS website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle the mobile menu
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
